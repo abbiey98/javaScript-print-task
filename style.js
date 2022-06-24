a798b60b9ab590cf91271d0de18a5e30cab13151
Yusuf Abiola Ronke <abiola.yusuf019@gmail.com>
2022-06-24 11:55:41 +0100

    first time pushing zuri print task to git

---
+++ style.js
@@ -0,0 +1,11 @@
+    const aboutMe = {
+    name: "Yusuf Abiola Ronke",
+    height: "5.8ft",
+    country: "Nigeria"
+};
+alert(aboutMe.name);
+alert(aboutMe.height);
+alert(aboutMe.country);
+
+
+console.log(aboutMe);
